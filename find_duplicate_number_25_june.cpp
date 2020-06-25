e2d91b1184240e4ee3eab615b6965e50a2441394
ajraj27 <rajanuj.iiita@gmail.com>
2020-06-25 13:51:56 +0530

    solution of findd duplicate number

---
+++ find_duplicate_number_25_june.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        
+        int slow=nums[0];
+        int fast=nums[0];
+        
+        while(1){
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+            
+            if(slow==fast) break;
+        }
+        
+        slow=nums[0];
+        
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        
+        return slow;
+        
+    }
+};
